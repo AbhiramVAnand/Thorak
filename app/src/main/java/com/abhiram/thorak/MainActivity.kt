eb088844c119035793e416db0ca507ee1d08a667
AbhiramVAnand <anandabhiram0.ar@gmail.com>
2022-09-03 09:42:41 +0530

    Created Clock #1

--- app/src/main/java/com/abhiram/thorak/MainActivity.kt
+++ app/src/main/java/com/abhiram/thorak/MainActivity.kt
@@ -3,12 +3,14 @@ package com.abhiram.thorak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.abhiram.thorak.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         supportActionBar?.hide()
         supportFragmentManager.beginTransaction().replace(R.id.frag_view,HomeFragment()).commit()
     }
--- app/src/main/java/com/abhiram/thorak/fragments/HomeFragment.kt
+++ app/src/main/java/com/abhiram/thorak/fragments/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.abhiram.thorak.fragments
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.*
+import android.widget.TextClock
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.abhiram.thorak.R
+import java.lang.reflect.Type
 
 class HomeFragment : Fragment() {
     /*
@@ -22,7 +24,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val inflate = inflater.inflate(R.layout.fragment_home, container, false)
+        val font : Typeface = Typeface.createFromAsset(context?.assets, "Fonts/NotoSans-Regular.ttf")
+        val time : TextClock = inflate.findViewById(R.id.time)
+        val date : TextClock = inflate.findViewById(R.id.date)
+        time.setTypeface(font)
+        date.setTypeface(font)
+        return inflate
     }
 
 }
